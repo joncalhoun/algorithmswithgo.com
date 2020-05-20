bdc6a8b018798c66016db4165fc628e0876659cc
zaryab2000 <zaryabafser2000@gmail.com>
2020-05-20 17:08:52 +0530

    solved using string to byte conversion

--- module01/fizz_buzz.go
+++ module01/fizz_buzz.go
@@ -1,4 +1,7 @@
 package module01
+import (
+	"fmt"
+)
 
 // FizzBuzz will print out all of the numbers
 // from 1 to N replacing any divisible by 3
@@ -10,5 +13,27 @@ package module01
 // `fmt` package and print to standard out.
 // I wouldn't normally recommend this, but did
 // it here to make life easier for beginners.
-func FizzBuzz(n int) {
+func FizzBuzz(number int) {
+	for i := 1; i < number;i++ {
+		if i % 3 == 0 && i%5 == 0 {
+			fmt.Print("Fizz Buzz", ", ")
+		}else if i % 3 ==0{
+			fmt.Print("Fizz", ", ")
+		}else if i%5 == 0{
+			fmt.Print("Buzz", ", ")
+		}else{
+			fmt.Print(i, ", ")
+		}
+	}
+	if number % 3 == 0 && number%5 == 0 {
+			fmt.Print("Fizz Buzz", "\n")
+		}else if number % 3 ==0{
+			fmt.Print("Fizz", "\n")
+		}else if number%5 == 0{
+			fmt.Print("Buzz", "\n")
+		}else{
+			fmt.Print(number, "\n")
+		}
+
+	
 }
