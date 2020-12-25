698e3e105adea7f225eb29ef095dae3e6b74cb0e
Chip Hudgins <mudzi42@gmail.com>
2020-12-24 17:59:51 -0800

    fibonacci

--- module01/fibonacci.go
+++ module01/fibonacci.go
@@ -25,5 +25,21 @@ package module01
 //   Fibonacci(14) => 377
 //
 func Fibonacci(n int) int {
-	return 0
+	// F(N) = F(N-1) + F(n-2)
+	// F(5) = F(4) + F(3)
+	// 13 = 8 + 5
+	// n > 1 ( 1 != 0 + -1)
+	// if n < 1 {
+	// 	return 0
+	// }
+	// if n == 1 {
+	// 	return 1
+	// }
+	// John's
+	if n <= 1 {
+		return n
+	}
+	//
+
+	return Fibonacci(n-1) + Fibonacci(n-2)
 }
