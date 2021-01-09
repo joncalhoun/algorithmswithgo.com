fcacd25ed30086044a2dc2e07d22bfbafc6053d6
Valeriy <valeriy.molchanov.77@gmail.com>
2021-01-09 20:10:44 +0200

    Features/reverse (#3)
    * Revers - multi-byte rune string
    
    * Revers - multi-rune character strings

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module algo
 
 go 1.13
+
+require golang.org/x/text v0.3.5
--- module01/reverse.go
+++ module01/reverse.go
@@ -1,5 +1,9 @@
 package module01
 
+import (
+	"golang.org/x/text/unicode/norm"
+)
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
@@ -7,5 +11,27 @@ package module01
 //   Reverse("alphabet") => "tebahpla"
 //
 func Reverse(word string) string {
-	return ""
+	b := []byte(word)
+	rb := make([]byte, len(b))
+
+	current := 0
+	for {
+		if current == len(b) {
+			break
+		}
+		next := norm.NFC.NextBoundary(b[current:], false)
+		if next == -1 {
+			next = len(b[current:])
+		}
+
+		for i := 0; i < next; i++ {
+			ri := len(rb) - current - next + i
+			rb[ri] = b[current+i]
+		}
+
+		current += next
+	}
+
+	reversedWord := string(rb)
+	return reversedWord
 }
