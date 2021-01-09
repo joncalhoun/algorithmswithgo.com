5f924914e9f8c18d692ff4c897393c38b9aa74ca
mova <valeriy.molchanov.77@gmail.com>
2021-01-09 16:49:04 +0200

    NumInList

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -3,5 +3,10 @@ package module01
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
+	for _, n := range list {
+		if n == num {
+			return true
+		}
+	}
 	return false
 }
